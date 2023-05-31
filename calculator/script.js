d183240dc1f7938783e7154872f30236037c5def
MrGentlemanSeb <sebgoleonardo3@gmail.com>
2023-05-31 11:10:34 +0000

    Calculatrice (v1)

---
+++ calculator/script.js
@@ -0,0 +1,14 @@
+
+function addToResult(value) {
+  document.getElementById('result').value += value;
+}
+
+function calculate() {
+  var result = document.getElementById('result').value;
+  var answer = eval(result);
+  document.getElementById('result').value = answer;
+}
+
+function clearResult() {
+  document.getElementById('result').value = '';
+}
